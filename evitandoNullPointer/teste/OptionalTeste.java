2baa6df382fb11fc4d3297cd3821b1697555853c
Renan Oliveira <renan.sergipe68@gmail.com>
2020-09-23 18:59:19 -0300

    Evitando o NullPointerException

---
+++ evitandoNullPointer/teste/OptionalTeste.java
@@ -0,0 +1,26 @@
+package br.com.abc.Java.evitandoNullPointer.teste;
+
+import java.util.Optional;
+
+public class OptionalTeste {
+    public String nome;
+
+    public static void main(String[] args) {
+        OptionalTeste op = new OptionalTeste ();
+
+        Optional < String > optional1 = Optional.of ( "renan" );//a variavel que eu passar no optional.of nao pode ser nula
+
+        Optional < String > optional2 = Optional.empty ();//se eu quiser iniciar o optional vazio
+
+        Optional < String > optional3 = Optional.ofNullable ( op.nome );//aqui é quando eu nao sei se a variavel é nula ou nao
+
+        //imprimindo os valores
+        System.out.println ( optional1 );
+        System.out.println ( optional2 );
+        System.out.println ( optional3 );
+        //buscando valores
+        System.out.println (optional1.get ());
+        System.out.println (optional1.orElse ( "vazio" ));//ele vai buscar a variavel, se nao tiver nada ele retorna oq tem dentro do orElse
+
+    }
+}
